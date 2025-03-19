703ec57ed84899efc3007e5103e490c913502900
Henok <hdbeleive@gmail.com>
2025-03-19 10:02:30 +0300

    display question component structure developed

--- word-seed/src/components/DisplayQuestion.jsx
+++ word-seed/src/components/DisplayQuestion.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const DisplayQuestion=() =>{
+    const words = useWordStore ((state) =>state.words);
+    const definition = words.find((w) => w.word===word)?.definition || "Fetching words..."
+
   return (
-    <div>DisplayQuestion</div>
-  )
-}
+    <div>
+        <p>{definition}</p>
+    </div>
+  );
+};
 
 export default DisplayQuestion
