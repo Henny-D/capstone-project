ca0f482fdb09cd1759877f2bb9ad57f9ee66e718
Henok <hdbeleive@gmail.com>
2025-03-19 09:35:58 +0300

    Home page structure developed

--- word-seed/src/components/Home.jsx
+++ word-seed/src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
 
 const Home=() => {
+    const navigate =useNavigate();
+
   return (
-    <div>Home</div>
-  )
+    <div>
+        <div>
+            <h1>Word <br/>Seed</h1>
+        </div>
+        <div>
+            <p>Play & Learn English!</p>
+            <p>A . B . C . D </p>
+            <button>START</button>
+        </div>
+    </div>
+  );
 }
 
 export default Home
