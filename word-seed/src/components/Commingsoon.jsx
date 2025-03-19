29a7d0c41d3b282ec755aea7a5c861e9c4bbf7cf
Henok <hdbeleive@gmail.com>
2025-03-19 11:11:43 +0300

    Comming Soon component structure developed

--- word-seed/src/components/Commingsoon.jsx
+++ word-seed/src/components/Commingsoon.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Commingsoon=() =>{
+const CommingSoon=() =>{
+    const navigate = useNavigate();
+
   return (
-    <div>Commingsoon</div>
-  )
+    <div>
+        <div>
+            <h1>Comming Soon!</h1>
+            <p>This game is currently under development. Stay tuned for updates!<br/> Thank You!</p>
+
+            <button onClick={()=> navigate('/GameMode')}>Back</button>
+        </div>
+    </div>
+  );
 }
 
-export default Commingsoon
+export default CommingSoon
