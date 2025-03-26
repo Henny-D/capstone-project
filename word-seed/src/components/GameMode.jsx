08165e26ae39fcec23b5abdb2acbc390af2e4b4e
Henok <hdbeleive@gmail.com>
2025-03-26 18:00:49 +0300

    tailwind finalized

--- word-seed/src/components/GameMode.jsx
+++ word-seed/src/components/GameMode.jsx
@@ -6,17 +6,17 @@ const GameMode=() => {
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-yellow-500 p-6 font-poppins'>
-        <h1 className='text-5xl font-bold text-zinc-800 mb-4'>PLAY</h1>
+        <h1 className='font-poppins text-5xl font-bold text-zinc-800 mb-4'>PLAY</h1>
 
         <div className=' text-center max-w-md w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mt-8'>
 
-            <button className='text-xl w-full mb-4 px-4 py-2 bg-cyan-600 text-zinc-700 font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300' 
+            <button className='font-poppins text-xl w-full mb-4 px-4 py-2 bg-cyan-600 text-zinc-700 font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300' 
             onClick={()=> navigate('/App')}>Vocabulary</button>
 
-            <button className='text-xl w-full mb-4 px-4 py-2 bg-cyan-600 text-zinc-700 font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300' 
+            <button className='font-poppins text-xl w-full mb-4 px-4 py-2 bg-cyan-600 text-zinc-700 font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300' 
              onClick={()=> navigate('/commingsoon')}>Antonym</button>
 
-            <button className='text-xl w-full mb-4 px-4 py-2 bg-cyan-600 text-zinc-700 font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300' 
+            <button className='font-poppins text-xl w-full mb-4 px-4 py-2 bg-cyan-600 text-zinc-700 font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300' 
             onClick={()=> navigate('/commingsoon')}>Synonym</button>
         </div>
     </div>
--- word-seed/src/components/Home.jsx
+++ word-seed/src/components/Home.jsx
@@ -11,11 +11,11 @@ const Home=() => {
             <h1 className='text-6xl font-bold text-zinc-700 mb-4'>Word <br/>Seed</h1>
         </div>
         <div className='text-center bg-zinc-300 rounded-full shadow-lg p-8 max-w-md w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mt-8'>
-            <p className='text-2xl font-semibold text-zinc-700 '>Play & Learn English!</p>
-            <p className='text-4xl font-bold text-zinc-700 mb-4'>A . B . C . D </p>
+            <p className=' font-poppins text-2xl font-semibold text-zinc-700 '>Play & Learn English!</p><br />
+            <p className='font-poppins text-4xl font-bold text-zinc-700 mb-4'>A . B . C . D </p><br />
             
             <button 
-            className='mb-0 px-2 py-2 bg-cyan-600 text-white font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300 '
+            className='font-poppins mb-0 px-2 py-2 bg-cyan-600 text-white font-semibold rounded-full shadow-md hover:bg-cyan-900 transition duration-300 '
             onClick={()=> navigate('/GameMode')}>START</button>
         </div>
     </div>
