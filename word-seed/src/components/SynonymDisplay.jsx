ca3e4385468314ee99402a9bd3cc540e633426d9
Henok <hdbeleive@gmail.com>
2025-04-01 11:29:10 +0300

    synonym display constructed

---
+++ word-seed/src/components/SynonymDisplay.jsx
@@ -0,0 +1,17 @@
+import React  from "react";
+import synonymModeStore from "../store/synonymModeStore";
+
+const SynonymDisplay = ({word}) => {
+    const words = synonymModeStore((state) => state.words);
+    const foundWord =words.find((item) => item.word === word);
+
+    return (
+        <div  className='bg-yellow-50 rounded-lg shadow-lg p-8 max-w-md w-full text-center font-poppins '>
+            <p  className='text-xl text-zinc-800 font-light'>
+                {foundWord ? foundWord.word : "Fetching Words"}
+            </p>
+        </div>
+    );
+};
+
+export default SynonymDisplay
